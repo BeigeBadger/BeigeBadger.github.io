3b99bb076b538cfa2deeff885f7e5c7970d5ba69
Matt Stannett <matt.stannett@gmail.com>
2021-12-02 22:40:34 +1300

    Adds under contruction message

--- my-cv/src/App.tsx
+++ my-cv/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
+					Under construction, check back soon.
 				</p>
 			</header>
 		</div>
