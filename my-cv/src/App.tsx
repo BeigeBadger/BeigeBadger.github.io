66953212c6614d49bd497ed6c06429a0aa821a18
Matt Stannett <matt.stannett@gmail.com>
2021-12-06 21:16:26 +1300

    Adds loading of resume from local file on initial page load

--- my-cv/src/App.tsx
+++ my-cv/src/App.tsx
@@ -1,7 +1,29 @@
 import logo from './images/Matt-Gravatar.png';
 import './App.scss';
+import { useEffect } from 'react';
+
 
 function App() {
+	useEffect(() => {
+		async function LoadResumeDetailsFromLocalFile() {
+			const resumeFileName = "resume.json";
+
+			const resumeJson =
+				await fetch(resumeFileName)
+					.then(function(response) {
+						return response.json();
+					})
+					.catch(function(reason) {
+						debugger;
+						// TODO: Handle the failure case
+					});
+
+			console.log(resumeJson);
+		}
+
+		LoadResumeDetailsFromLocalFile();
+	 }, []);
+
 	return (
 		<div className="App">
 			<header className="App-header">
