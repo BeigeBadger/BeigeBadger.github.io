bc62906bb03d171aeb453f522774aec7d1d868cb
Matt Stannett <matt.stannett@gmail.com>
2022-01-10 17:21:37 +1300

    Adds emphasis to achievements

--- my-cv/src/components/workHistory.tsx
+++ my-cv/src/components/workHistory.tsx
@@ -61,10 +61,7 @@ const WorkHistory: React.FC<IWorkHistoryProps> = (props: IWorkHistoryProps) => {
 													{
 														job.highlights.map((highlight, index) => {
 															return (
-																<li key={index}>
-																	{
-																		highlight
-																	}
+																<li key={index} dangerouslySetInnerHTML={{ __html: highlight }}>
 																</li>
 															)
 														})
