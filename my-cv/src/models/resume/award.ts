dcc3a8d6fb62fa5bb1407452ebcf644a1015d3c9
Matt Stannett <matt.stannett@gmail.com>
2025-09-05 22:13:09 +1200

    refactor: cleans up awards model

--- my-cv/src/models/resume/award.ts
+++ my-cv/src/models/resume/award.ts
@@ -3,10 +3,8 @@ import { IYearMonthDay } from "./yearMonthDay";
 export interface IAward {
 	title: string;
 	date: string;
-	type: string;
 	awarder: string;
 	summary: string;
-	fullDate: IYearMonthDay;
 	link: string;
 	display: boolean;
 }
