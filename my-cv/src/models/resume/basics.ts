026fdf11c1150a085bda5842351dbd05db0a7a8d
Matt Stannett <matt.stannett@gmail.com>
2025-09-05 21:08:46 +1200

    chore: removes unused keys in basics section

--- my-cv/src/models/resume/basics.ts
+++ my-cv/src/models/resume/basics.ts
@@ -3,23 +3,13 @@ import { IProfile } from "./profile";
 export interface IBasics {
 	name: string;
 	jobTitle: string;
-	image: string;
-	email: string;
-	phone: string;
-	url?: any;
 	summary: string;
 	profiles: IProfile[];
 	headline: string;
 	blog?: any;
-	yearsOfExperience: number;
 	fullTimeExperienceStartDate: string;
-	username: string;
 	locationAsString: string;
 	location: string;
-	karma: number;
 	id: string;
-	followers: number;
-	following: number;
 	picture: string;
-	website?: any;
 }
