c4a49c2547ded4053c482c4f7204899b79ed0207
Matt Stannett <matt.stannett@gmail.com>
2025-09-05 22:11:01 +1200

    refactor: tidies and simplifies quals section

--- my-cv/src/models/resume/qualification.ts
+++ my-cv/src/models/resume/qualification.ts
@@ -2,17 +2,11 @@ import { IYearMonth } from "./yearMonth";
 
 export interface IQualification {
 	institution: string;
-	url: string;
 	area: string;
 	studyType: string;
-	startDate: string;
-	endDate: string;
 	endorsement: string;
-	courses: any[];
-	description: string;
-	activities: string;
 	start: IYearMonth;
 	end: IYearMonth;
 	website: string;
-	gpa: string;
+	result: string;
 }
