ca3c05a72eccdeb7f18b711f16f09501fb756ae5
Matt Stannett <matt.stannett@gmail.com>
2025-09-07 21:18:19 +1200

    chore: reorder props for consistency

--- my-cv/src/models/resume/jobRole.ts
+++ my-cv/src/models/resume/jobRole.ts
@@ -1,15 +1,15 @@
 import { WorkModelEnum } from "../workModelEnum";
 
 export interface IJobRole {
-	location: string;
-	responsibilities: string[];
-	position: string;
-	startDate: string;
-	endDate: string;
-	highlights: string[];
-	start: string;
-	end: string;
 	company: string;
+	position: string;
 	website: string;
+	startDate: string;
+	start: string;
+	endDate: string;
+	end: string;
+	location: string;
 	workModel: WorkModelEnum;
+	responsibilities: string[];
+	highlights: string[];
 }
